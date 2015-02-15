ba94a97b6753f24e76248a02964726f5e291bf85
Robby Ranshous <rranshous@gmail.com>
2015-02-15 18:49:03 -0500

    initial script to download new images

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'eventstore', '~> 0.1'
+gem 'sequel'
+gem 'pg', group: :production
+gem 'sqlite3', group: :development
+gem 'pry', group: :development
---
+++ app.rb
@@ -0,0 +1,63 @@
+require 'eventstore'
+require 'sequel'
+require 'base64'
+require 'json'
+
+CONNSTRING = ARGV.shift || 'http://0.0.0.0:2113'
+WRITE_DIR = ARGV.shift || './data'
+
+database_url = ENV['DATABASE_URL'] || "sqlite://dev.db"
+DB = Sequel.connect(database_url, :encoding => 'utf-8')
+
+begin
+  DB.create_table :images do
+    primary_key :id
+    String      :path
+    String      :href
+    timestamp   :created_at
+
+    index       :href, :unique => true
+  end
+rescue Sequel::DatabaseError
+  puts "IMAGES TABLE EXISTS"
+end
+
+class Image < Sequel::Model
+  def data
+    File.read(path)
+  end
+end
+
+SLEEP_TIME = 10
+start_at = 0
+last_start_at = nil
+loop do
+  if last_start_at == start_at
+    puts "SLEEPING: #{SLEEP_TIME}"
+    sleep SLEEP_TIME
+  end
+  last_start_at = start_at
+  eventstore = EventStore::Client.new(CONNSTRING)
+  puts "START_AT: #{start_at}"
+  events = eventstore.resume_read('new-images', start_at, 100)
+  events.each do |event|
+    href = event[:body]["href"]
+    puts "CHECKING: #{href}"
+    image = Image.where(href: href).first
+    if image
+      puts "FOUND IMAGE: #{image}"
+    else
+      puts "DOWNLOADING: #{href}"
+      response = HTTParty.get(href)
+      image_data = response.parsed_response
+      filename = Base64.encode64(href)
+      out_path = File.join WRITE_DIR, filename
+      puts "WRITING: #{out_path}"
+      File.write(out_path, image_data)
+      image = Image.new(href: href, path: out_path)
+      image.save
+      puts "SAVED: #{image.id} :: #{href} :: #{out_path}"
+    end
+    start_at = event[:id]
+  end
+end
